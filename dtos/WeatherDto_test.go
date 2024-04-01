94ba2276de990ce66c97b87bf7584256e182abe1
Alexander A <alexander.asbeck@stud.th-rosenheim.de>
2024-04-01 15:56:37 +0200

    Testing Equals and Diff Method in WeatherDto

--- dtos/WeatherDto_test.go
+++ dtos/WeatherDto_test.go
@@ -61,3 +61,223 @@ func TestWeatherDto_String(t *testing.T) {
 		})
 	}
 }
+
+func TestWeatherDto_Equals(t *testing.T) {
+	type fields struct {
+		Location    string
+		Temperature string
+		Humidity    string
+		SunHours    int
+		WindSpeed   string
+		Weather     string
+		Date        string
+	}
+	type args struct {
+		other *WeatherDto
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		{
+			name: "Test Berlin Equals()",
+			fields: fields{
+				Location:    "Berlin",
+				Temperature: "20°C",
+				Humidity:    "50%",
+				SunHours:    8,
+				WindSpeed:   "5m/s",
+				Weather:     "Sunny",
+				Date:        "2021-09-01",
+			},
+			args: args{
+				other: &WeatherDto{
+					Location:    "Berlin",
+					Temperature: "20°C",
+					Humidity:    "50%",
+					SunHours:    8,
+					WindSpeed:   "5m/s",
+					Weather:     "Sunny",
+					Date:        "2021-09-01",
+				},
+			},
+			want: true,
+		},
+		{
+			name: "Test Paris Equals()",
+			fields: fields{
+				Location:    "Paris",
+				Temperature: "25°C",
+				Humidity:    "60%",
+				SunHours:    10,
+				WindSpeed:   "3m/s",
+				Weather:     "Cloudy",
+				Date:        "2021-09-01",
+			},
+			args: args{
+				other: &WeatherDto{
+					Location:    "Paris",
+					Temperature: "25°C",
+					Humidity:    "60%",
+					SunHours:    10,
+					WindSpeed:   "3m/s",
+					Weather:     "Cloudy",
+					Date:        "2021-09-01",
+				},
+			},
+			want: true,
+		},
+		{
+			name: "Test Berlin and Paris Equals()",
+			fields: fields{
+				Location:    "Berlin",
+				Temperature: "20°C",
+				Humidity:    "50%",
+				SunHours:    8,
+				WindSpeed:   "5m/s",
+				Weather:     "Sunny",
+				Date:        "2021-09-01",
+			},
+			args: args{
+				other: &WeatherDto{
+					Location:    "Paris",
+					Temperature: "25°C",
+					Humidity:    "60%",
+					SunHours:    10,
+					WindSpeed:   "3m/s",
+					Weather:     "Cloudy",
+					Date:        "2021-09-01",
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			wd := WeatherDto{
+				Location:    tt.fields.Location,
+				Temperature: tt.fields.Temperature,
+				Humidity:    tt.fields.Humidity,
+				SunHours:    tt.fields.SunHours,
+				WindSpeed:   tt.fields.WindSpeed,
+				Weather:     tt.fields.Weather,
+				Date:        tt.fields.Date,
+			}
+			if got := wd.Equals(tt.args.other); got != tt.want {
+				t.Errorf("Equals() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWeatherDto_Diff(t *testing.T) {
+	type fields struct {
+		Location    string
+		Temperature string
+		Humidity    string
+		SunHours    int
+		WindSpeed   string
+		Weather     string
+		Date        string
+	}
+	type args struct {
+		other *WeatherDto
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   string
+	}{
+		{
+			name: "Test Berlin Diff()",
+			fields: fields{
+				Location:    "Berlin",
+				Temperature: "20°C",
+				Humidity:    "50%",
+				SunHours:    8,
+				WindSpeed:   "5m/s",
+				Weather:     "Sunny",
+				Date:        "2021-09-01",
+			},
+			args: args{
+				other: &WeatherDto{
+					Location:    "Berlin",
+					Temperature: "20°C",
+					Humidity:    "50%",
+					SunHours:    8,
+					WindSpeed:   "5m/s",
+					Weather:     "Sunny",
+					Date:        "2021-09-01",
+				},
+			},
+			want: "",
+		},
+		{
+			name: "Test Paris Diff()",
+			fields: fields{
+				Location:    "Paris",
+				Temperature: "25°C",
+				Humidity:    "60%",
+				SunHours:    10,
+				WindSpeed:   "3m/s",
+				Weather:     "Cloudy",
+				Date:        "2021-09-01",
+			},
+			args: args{
+				other: &WeatherDto{
+					Location:    "Paris",
+					Temperature: "25°C",
+					Humidity:    "60%",
+					SunHours:    10,
+					WindSpeed:   "3m/s",
+					Weather:     "Cloudy",
+					Date:        "2021-09-01",
+				},
+			},
+			want: "",
+		},
+		{
+			name: "Test Berlin and Paris Diff()",
+			fields: fields{
+				Location:    "Berlin",
+				Temperature: "20°C",
+				Humidity:    "50%",
+				SunHours:    8,
+				WindSpeed:   "5m/s",
+				Weather:     "Sunny",
+				Date:        "2021-09-01",
+			},
+			args: args{
+				other: &WeatherDto{
+					Location:    "Paris",
+					Temperature: "25°C",
+					Humidity:    "60%",
+					SunHours:    10,
+					WindSpeed:   "3m/s",
+					Weather:     "Cloudy",
+					Date:        "2022-09-01",
+				},
+			},
+			want: "Location: Berlin != Paris\nTemperature: 20°C != 25°C\nHumidity: 50% != 60%\nSunHours: 8 != 10\nWindSpeed: 5m/s != 3m/s\nWeather: Sunny != Cloudy\nDate: 2021-09-01 != 2022-09-01\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			wd := WeatherDto{
+				Location:    tt.fields.Location,
+				Temperature: tt.fields.Temperature,
+				Humidity:    tt.fields.Humidity,
+				SunHours:    tt.fields.SunHours,
+				WindSpeed:   tt.fields.WindSpeed,
+				Weather:     tt.fields.Weather,
+				Date:        tt.fields.Date,
+			}
+			if got := wd.Diff(tt.args.other); got != tt.want {
+				t.Errorf("Diff() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
