771457921f28109a555fbe2ad5a46654169f2629
Alexander A <alexander.asbeck@stud.th-rosenheim.de>
2024-04-06 23:15:58 +0200

    Adding Tests for Unclosable Body

--- service/WeatherService_test.go
+++ service/WeatherService_test.go
@@ -7,12 +7,31 @@ import (
 	"io"
 	"net/http"
 	"testing"
+	"testing/iotest"
 
 	"codeberg.org/Birkenfunk/SQS/dtos"
 	"codeberg.org/Birkenfunk/SQS/mocks"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
+
+type NonClosableBody struct {
+	data  []byte
+	index int
+}
+
+func (b *NonClosableBody) Close() error {
+	return err
+}
+
+func (b *NonClosableBody) Read(_ []byte) (n int, err error) {
+	if b.index >= len(b.data) {
+		return 0, io.EOF
+	}
+	n = copy(b.data, b.data[b.index:])
+	b.index += n
+	return n, nil
+}
 
 type WeatherServiceSuite struct {
 	suite.Suite
@@ -99,6 +118,38 @@ func (suite *WeatherServiceSuite) TestGetWeather_FailUnmarshal() {
 	suite.Error(err)
 }
 
+func (suite *WeatherServiceSuite) TestGetWeather_FailClose() {
+	// given:
+	dto, err := json.Marshal(suite.dto)
+	suite.Require().NoError(err)
+	body := &NonClosableBody{data: dto}
+	response := http.Response{Body: body, StatusCode: 200}
+	suite.mockClient.On("Do", mock.Anything).Return(&response, nil)
+	suite.mockClient.On("Do", mock.Anything).Return(nil, err)
+
+	// when:
+	result, err := suite.service.GetWeather("Berlin")
+
+	// then:
+	suite.Nil(result)
+	suite.Error(err)
+}
+
+func (suite *WeatherServiceSuite) TestGetWeather_FailReadAll() {
+	// given:
+	body := io.NopCloser(iotest.ErrReader(err))
+	response := http.Response{Body: body, StatusCode: 200}
+	suite.mockClient.On("Do", mock.Anything).Return(&response, nil)
+	suite.mockClient.On("Do", mock.Anything).Return(nil, err)
+
+	// when:
+	result, err := suite.service.GetWeather("Berlin")
+
+	// then:
+	suite.Nil(result)
+	suite.Error(err)
+}
+
 func (suite *WeatherServiceSuite) TestGetHealth_Success() {
 	// given:
 	body := io.NopCloser(bytes.NewReader([]byte("OK")))
@@ -114,6 +165,33 @@ func (suite *WeatherServiceSuite) TestGetHealth_Success() {
 
 func (suite *WeatherServiceSuite) TestGetHealth_Fail() {
 	// given:
+	suite.mockClient.On("Do", mock.Anything).Return(nil, err)
+
+	// when:
+	err := suite.service.GetHealth()
+
+	// then:
+	suite.Error(err)
+}
+
+func (suite *WeatherServiceSuite) TestGetHealth_FailStatusCode() {
+	// given:
+	body := io.NopCloser(bytes.NewReader([]byte("error")))
+	response := http.Response{Body: body, StatusCode: 500}
+	suite.mockClient.On("Do", mock.Anything).Return(&response, nil)
+
+	// when:
+	err := suite.service.GetHealth()
+
+	// then:
+	suite.Error(err)
+}
+
+func (suite *WeatherServiceSuite) TestGetHealth_FailClose() {
+	// given:
+	body := &NonClosableBody{data: []byte("OK")}
+	response := http.Response{Body: body, StatusCode: 200}
+	suite.mockClient.On("Do", mock.Anything).Return(&response, nil)
 	suite.mockClient.On("Do", mock.Anything).Return(nil, err)
 
 	// when:
