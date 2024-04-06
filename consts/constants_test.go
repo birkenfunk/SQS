88ef94f072d51cd7ebd309b025a61db04e81e30a
Alexander A <alexander.asbeck@stud.th-rosenheim.de>
2024-04-06 19:00:39 +0200

    Adding Testify for testing

--- consts/constants_test.go
+++ consts/constants_test.go
@@ -1,101 +1,61 @@
 package consts
 
-import "testing"
+import (
+	"testing"
 
-func TestGetPort(t *testing.T) {
-	tests := []struct {
-		name string
-		set  string
-		want string
-	}{
-		{
-			name: "Test GetPort",
-			set:  "8080",
-			want: "8080",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			port = tt.set
-			if got := GetPort(); got != tt.want {
-				t.Errorf("GetPort() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	"github.com/stretchr/testify/suite"
+)
+
+type ConstantsSuite struct {
+	suite.Suite
 }
 
-func TestGetWeatherServiceURL(t *testing.T) {
-	tests := []struct {
-		name string
-		set  string
-		want string
-	}{
-		{
-			name: "Test GetWeatherServiceURL",
-			set:  "http://localhost:8080",
-			want: "http://localhost:8080",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			weatherServiceURL = tt.set
-			if got := GetWeatherServiceURL(); got != tt.want {
-				t.Errorf("GetWeatherServiceURL() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+func TestConstantsSuite(t *testing.T) {
+	suite.Run(t, &ConstantsSuite{})
 }
 
-func TestSetPortFromString(t *testing.T) {
-	type args struct {
-		p string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "Test SetPortFromString",
-			args: args{
-				p: "8080",
-			},
-			want: "8080",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			SetPortFromString(tt.args.p)
-			if port != tt.want {
-				t.Errorf("SetPortFromString() has set Port to %v, but should be %v", port, tt.want)
-			}
-		})
-	}
+func (suite *ConstantsSuite) TestGetPort() {
+	// given:
+	port = "8080"
+	SetPortFromString(port)
+
+	// when:
+	result := GetPort()
+
+	// then:
+	suite.Equal(port, result)
 }
 
-func TestSetWeatherServiceURL(t *testing.T) {
-	type args struct {
-		url string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{
-			name: "Test SetWeatherServiceURL",
-			args: args{
-				url: "http://localhost:8080",
-			},
-			want: "http://localhost:8080",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			SetWeatherServiceURL(tt.args.url)
-			if weatherServiceURL != tt.want {
-				t.Errorf("SetWeatherServiceURL() has set WeatherServiceURL to %v, but should be %v", weatherServiceURL, tt.want)
-			}
-		})
-	}
+func (suite *ConstantsSuite) TestGetWeatherServiceURL() {
+	// given:
+	weatherServiceURL = "http://localhost:8080"
+	SetWeatherServiceURL(weatherServiceURL)
+
+	// when:
+	result := GetWeatherServiceURL()
+
+	// then:
+	suite.Equal(weatherServiceURL, result)
+}
+
+func (suite *ConstantsSuite) TestSetPortFromString() {
+	// given:
+	port = "8080"
+
+	// when:
+	SetPortFromString(port)
+
+	// then:
+	suite.Equal(port, GetPort())
+}
+
+func (suite *ConstantsSuite) TestSetWeatherServiceURL() {
+	// given:
+	weatherServiceURL = "http://localhost:8080"
+
+	// when:
+	SetWeatherServiceURL(weatherServiceURL)
+
+	// then:
+	suite.Equal(weatherServiceURL, GetWeatherServiceURL())
 }
