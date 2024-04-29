6ce793b20690c1afd256bb9fe25c0d8370a040ab
Alexander <alexander.asbeck@stud.th-rosenheim.de>
2024-04-29 09:46:23 +0200

    Adding Cors

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
--- presentation/router.go
+++ presentation/router.go
@@ -4,6 +4,7 @@ import (
 	"codeberg.org/Birkenfunk/SQS/business/handler"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 type IRouter interface {
@@ -33,6 +34,16 @@ func (router *Router) InitRouter() *chi.Mux {
 	r.Use(middleware.Heartbeat("/"))
 	r.Use(middleware.RealIP)
 	r.Use(middleware.RedirectSlashes)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	r.Mount("/api", router.apiRouter())
 
